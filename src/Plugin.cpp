7f30ee0c1620a045423291a8f4267f7e0e17595d
Tudor Butufei <tudor.butufei@gmail.com>
2021-12-06 03:07:36 +0200

    add copyright notice

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -1,3 +1,13 @@
+/* Copyright (C) 2021 Tudor Butufei
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #include "Windows.h"
 #include "../API/RainmeterAPI.h"
 #include "MediaPlayer.h"
